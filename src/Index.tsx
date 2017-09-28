3042c056acc40ffaef05f992337943d11497df8e
wk-j <somnuk.wk@gmail.com>
2017-09-29 00:01:39 +0700

    Tree beard

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Demo } from "./Demo";
+import { TreeExample } from "./TreeBeard";
 
-ReactDOM.render(<Demo />, document.getElementById("app"));
+ReactDOM.render(<TreeExample />, document.getElementById("app"));
---
+++ src/TreeBeard.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Treebeard} from 'react-treebeard';
+
+const data = {
+    name: 'root',
+    toggled: true,
+    children: [
+        {
+            name: 'parent',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        },
+        {
+            name: 'loading parent',
+            loading: true,
+            children: []
+        },
+        {
+            name: 'parent',
+            children: [
+                {
+                    name: 'nested parent',
+                    children: [
+                        { name: 'nested child 1' },
+                        { name: 'nested child 2' }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+type State = {
+    cursor: any;
+}
+
+export class TreeExample extends React.Component<{}, State> {
+    constructor(props){
+        super(props);
+        this.state = { cursor: {} };
+        this.onToggle = this.onToggle.bind(this);
+    }
+    onToggle(node, toggled){
+        if(this.state.cursor){this.state.cursor.active = false;}
+        node.active = true;
+        if(node.children){ node.toggled = toggled; }
+        this.setState({ cursor: node });
+    }
+    render(){
+        return (
+            <Treebeard
+                data={data}
+                onToggle={this.onToggle}
+            />
+        );
+    }
+}
