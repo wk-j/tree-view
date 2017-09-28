7e5c8f2e36a8500d398d3986087d0a111d91faf4
wk-j <somnuk.wk@gmail.com>
2017-09-29 00:11:35 +0700

    Test rc-tree

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Demo } from "./Demo";
+
+import { Demo } from "./TreeView";
 import { TreeExample } from "./TreeBeard";
+import { demo } from "./RcTree";
 
 ReactDOM.render(<TreeExample />, document.getElementById("app"));
+ReactDOM.render(demo, document.getElementById("rc-tree"));
