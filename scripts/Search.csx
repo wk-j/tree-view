d546d68b1e383ca7e8211dbf2414d1664e67b4a0
wk-j <somnuk.wk@gmail.com>
2018-07-19 16:31:45 +0700

    Removed dir

---
+++ scripts/Search.csx
@@ -0,0 +1,5 @@
+using System.IO;
+
+var files = new DirectoryInfo(".").GetFiles("*.json", SearchOption.AllDirectories);
+
+
--- src/TreeView/Controllers/TreeController.cs
+++ src/TreeView/Controllers/TreeController.cs
@@ -9,20 +9,29 @@ namespace TreeView.Controllers {
     [Route("api/[controller]/[action]")]
     public class TreeController : ControllerBase {
 
+        string[] patterns = { "*.dll" };
+
+        private bool ContainFiles(DirectoryInfo info) {
+            return patterns.Any(x => info.GetFiles(x, SearchOption.AllDirectories).Count() > 0);
+        }
+
+        private IEnumerable<FileInfo> GetFiles(DirectoryInfo info) {
+            return patterns.Select(x => info.GetFiles(x)).SelectMany(x => x);
+        }
+
         private IEnumerable<Node> FindNode(DirectoryInfo root) {
-            foreach (var file in root.GetFiles()) {
-                if (file.Name.EndsWith(".json")) {
-                    yield return new Node {
-                        IsRoot = false,
-                        Id = file.FullName.GetHashCode(),
-                        Name = file.Name,
-                        IsFile = true,
-                        Parent = root.FullName.GetHashCode()
-                    };
-                }
+
+            foreach (var file in GetFiles(root)) {
+                yield return new Node {
+                    IsRoot = false,
+                    Id = file.FullName.GetHashCode(),
+                    Name = file.Name,
+                    IsFile = true,
+                    Parent = root.FullName.GetHashCode()
+                };
             }
 
-            foreach (var item in root.GetDirectories()) {
+            foreach (var item in root.GetDirectories().Where(ContainFiles)) {
                 yield return new Node {
                     IsRoot = false,
                     Id = item.FullName.GetHashCode(),
